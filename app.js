7f14650035c2c0fc0232aa5f6dfdd5853334dcb4
iamchrisas <chrisas.agb@gmail.com>
2023-12-19 16:52:36 +0100

    done
    got some errors regarding the size of the packages

--- app.js
+++ app.js
@@ -1,25 +1,51 @@
 const express = require('express');
-
-const hbs = require('hbs');
 const path = require('path');
+const expressHbs = require('express-handlebars');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
 
+// Setup Handlebars
+app.engine(
+  'hbs',
+  expressHbs({
+    defaultLayout: 'layout',
+    extname: '.hbs'
+  })
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
+// Home route
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+// Beers route
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beers: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+// Random Beer route
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      const [beer] = responseFromAPI; // Since it's an array with one object, destructure it to get the object.
+      res.render('random-beer', { beer });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
